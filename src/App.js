5064885fed585652ba36227c252dfa7986d69121
Aryan Singh <140090637+AryanSingh04@users.noreply.github.com>
2024-01-07 21:42:43 +0530

    Update App.js
    Added a scroll to top button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import Navbar from './components/Navbar';
+import { lazy, Suspense,useState,useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FaArrowUp } from "react-icons/fa";
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
-import { useRef } from 'react';
-import Contact from './pages/Contact';
-import Vehicle from './pages/Vehicle';
-import Testmonal from './pages/Testmonal';
 import Footer from './components/Footer';
+const Contact = lazy(()=>import('./pages/Contact'))
+const About = lazy(()=> import( './pages/About'))
+const Vehicle = lazy(()=> import('./pages/Vehicle'))
+const Testmonal = lazy(()=> import("./pages/Testmonal"))
+
 
 function App() {
+  const [scroll,setScroll] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const viewportHeight = window.innerHeight;
+      setScroll(scrollPosition > viewportHeight ? true:false);
+      console.log(scrollPosition,viewportHeight,scroll)
+    };
+    document.addEventListener('scroll', handleScroll);
+  }, []);
   return (
     <div className="w-[100vw] h-fit overflow-x-hidden">
-    
       <BrowserRouter>
       <Navbar />
+      { scroll &&
+        <div className="w-14 aspect-square bg-orange-600 rounded-full fixed bottom-4 right-8 flex items-center justify-center hover:bg-orange-700 cursor-pointer" onClick={()=>window.scrollTo(0,0)}>
+          <FaArrowUp size={25} color='white' className='anim'/>
+        </div>
+      }
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/vehicle' element={<Vehicle/>} />
-          <Route path='/testimonal' element={<Testmonal/>} />
+          <Route path='/about' element={<Suspense fallback={<h3>Loading...</h3>}>  <About /></Suspense>
+         } />
+          <Route path='/contact' element={<Suspense fallback={<h3>Loading...</h3>}>  <Contact /></Suspense>
+         } />
+          <Route path='/vehicle' element={<Suspense fallback={<h3>Loading...</h3>}>  <Vehicle/></Suspense>
+         } />
+          <Route path='/testimonal' element={<Suspense fallback={<h3>Loading...</h3>}> <Testmonal/></Suspense>
+         } />
         </Routes>
       </BrowserRouter>
      <Footer/>
